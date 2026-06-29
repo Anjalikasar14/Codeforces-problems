efab167195005077e1baedb52d8ebff9a963bd87
Anjali <anjalikasar06@gmail.com>
2026-06-29 11:32:01 +0530

    1878A

---
+++ 1878A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n,k;
+    cin>>n>>k;
+  
+
+  vector<int> arr(n);
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  bool found = false;
+  for(int i=0;i<n;i++){
+    if(arr[i]==k){
+      found = true;
+    } 
+  }
+
+  if(found){
+    cout<<"YES";
+  }else{
+    cout<<"NO";
+  }
+
+  }
+  return 0;
+}
